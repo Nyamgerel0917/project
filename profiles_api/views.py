a92d05bdf6ac7c4f5a686742afbe01f84a705531
Nyamgerel <nyamgerel.badraa@gmail.com>
2022-11-14 09:43:42 +0800

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,29 +4,28 @@ from rest_framework import status
 
 from profiles_api import serializers
 
-
 class HelloApiView(APIView):
-     """Test API View"""
-     serializer_class = serializer.HelloSerializer
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
-     def get(self, request, format=None):
-         """Returns a list of APIView features"""
-         an_apiview = [
-         'Uses HTTP methods as function (get,post,patch,put,delete)',
-         'Is similar to a traditional Django View',
-         'Gives you the most control over you appplicition logic',
-         'Is mapper manaully tu URLs',
-         ]
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLs',
+        ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
-     def post(self, request):
+    def post(self, request):
         """Create a hello message with our name"""
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
-            message = f'Hello {name}!'
+            message = f'Hello {name}'
             return Response({'message': message})
         else:
             return Response(
@@ -34,19 +33,14 @@ class HelloApiView(APIView):
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        def put(self,request,pk=None):
-            """Handle updating an object"""
-            return Response({'method':'PUT'})
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
 
-        def patch(self, request, pk=None):
-            """Handle a partial update of an object"""
-            return Response({'method': 'PUT'})
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
 
-        def patch(self,request, pk=None):
-            """Handle a partial update of an object"""
-            return Response({'method':'PATCH'})
-
-        def delete(self,request, pk=None):
-            """Delete an object"""
-            return Response({'method': 'DELETE'})
-            
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from django.contrib import admin
+from profiles_api import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
